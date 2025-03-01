a0ef8d71dafc2445c6a242a4d229c8c41880e7ca
NadishiChristina <nadishiramanayake@gmail.com>
2025-03-01 13:57:47 +0530

    Progress Analytics FE - Vite
    The Frontend made using React.js+Vite

---
+++ progress-analytics-frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import AnalyticsDashboard from "./components/AnalyticsDashboard"
+
+function App() {
+  return (
+    <div className="container mt-4">
+      <h1 className="mb-4">achieve+ Analytics Dashboard</h1>
+      <AnalyticsDashboard />
+    </div>
+  )
+}
+
+export default App
+
