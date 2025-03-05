95b10731eb09c761093942aecff76635be4ae562
NadishiChristina <nadishiramanayake@gmail.com>
2025-03-05 10:46:45 +0530

    Commiting Analytics classes
    Modified to get specific user metrics

--- ProgressAnalyticsBE/ProgressAnalyticsBE/src/main/java/com/SDGP/ProgressAnalyticsBE/controller/AnalyticsController.java
+++ ProgressAnalyticsBE/ProgressAnalyticsBE/src/main/java/com/SDGP/ProgressAnalyticsBE/controller/AnalyticsController.java
@@ -2,10 +2,10 @@ package com.SDGP.ProgressAnalyticsBE.controller;
 
 import com.SDGP.ProgressAnalyticsBE.model.ChartData;
 import com.SDGP.ProgressAnalyticsBE.model.TaskMetrics;
+import com.SDGP.ProgressAnalyticsBE.model.UserDto;
 import com.SDGP.ProgressAnalyticsBE.service.AnalyticsService;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -17,12 +17,25 @@ public class AnalyticsController {
     private AnalyticsService analyticsService;
 
     @GetMapping("/metrics")
-    public TaskMetrics getMetrics(@RequestParam String dateRange) {
-        return analyticsService.getMetrics(dateRange);
+    public TaskMetrics getMetrics(@RequestParam String dateRange, @RequestParam(required = false) Long userId) {
+        if (userId != null) {
+            return analyticsService.getMetricsByUser(dateRange, userId);
+        } else {
+            return analyticsService.getMetrics(dateRange);
+        }
     }
 
     @GetMapping("/chart-data")
-    public ChartData getChartData(@RequestParam String dateRange) {
-        return analyticsService.getChartData(dateRange);
+    public ChartData getChartData(@RequestParam String dateRange, @RequestParam(required = false) Long userId) {
+        if (userId != null) {
+            return analyticsService.getChartDataByUser(dateRange, userId);
+        } else {
+            return analyticsService.getChartData(dateRange);
+        }
+    }
+
+    @GetMapping("/users")
+    public List<UserDto> getAllUsers() {
+        return analyticsService.getAllUsers();
     }
 }
