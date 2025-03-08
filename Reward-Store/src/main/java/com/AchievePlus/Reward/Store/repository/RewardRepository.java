0b72c61e4aaf6ea54f10a0fa287aae409eef52f6
Pasindu Ravisara <121508486+Rexa444@users.noreply.github.com>
2025-03-08 10:33:29 +0530

    Delete Reward-Store directory

--- Reward-Store/src/main/java/com/AchievePlus/Reward/Store/repository/RewardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.AchievePlus.Reward.Store.repository;
-
-import com.AchievePlus.Reward.Store.model.Reward;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RewardRepository extends JpaRepository<Reward, Long> {
-}
--- Reward-Store/target/classes/data.sql
+++
@@ -1,8 +0,0 @@
--- Insert sample rewards
-INSERT INTO REWARD (name, points_cost, description, image_url) VALUES
-('Smartphone', 10000, 'Latest model smartphone', '/images/smartphone.jpg'),
-('Movie Ticket', 1000, 'Cinematic experience', '/images/movie-ticket.jpg'),
-('Cash Reward', 200, 'Direct cash reward', '/images/cash.jpg');
-
--- Insert a sample user
-INSERT INTO USER (username, total_points) VALUES ('testuser', 12345);
