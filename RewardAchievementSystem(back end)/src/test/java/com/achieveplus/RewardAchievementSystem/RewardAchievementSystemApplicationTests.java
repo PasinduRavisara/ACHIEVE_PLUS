33831777c49cf5cf3dc9ae5cd84a2cc8d6c81281
Devdun Fernando <devfernando222@gmail.com>
2025-03-10 13:44:43 +0530

    Rewards and Achievements(back end)

---
+++ RewardAchievementSystem(back end)/src/test/java/com/achieveplus/RewardAchievementSystem/RewardAchievementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.achieveplus.RewardAchievementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardAchievementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
