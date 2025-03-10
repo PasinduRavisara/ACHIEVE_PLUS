325c56430dc5c9d7506e9ec23766e04338a2536f
Devdun Fernando <devfernando222@gmail.com>
2025-03-10 14:30:40 +0530

    Application update

---
+++ RewardAchievementSystem(back end)/src/main/java/com/achieveplus/RewardAchievementSystem/application/RewardAchievementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.achieveplus.RewardAchievementSystem.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardAchievementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardAchievementSystemApplication.class, args);
+	}
+
+}
