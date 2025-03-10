fcc5af2296305040630f38237c18396669493c18
Devdun Fernando <devfernando222@gmail.com>
2025-03-10 14:08:40 +0530

    DTO update

---
+++ RewardAchievementSystem(back end)/dto/CompletionRateDTO.java
@@ -0,0 +1,10 @@
+package com.achieveplus.RewardAchievementSystem.dto;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+
+@Data
+@AllArgsConstructor
+public class CompletionRateDTO {
+    private double completionRate;
+}
