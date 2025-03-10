910adb5230f9b975a58d2ddcc5eca42efef6c901
Devdun Fernando <devfernando222@gmail.com>
2025-03-10 14:26:14 +0530

    Service

--- RewardAchievementSystem(back end)/repository/RewardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.achieveplus.RewardAchievementSystem.repository;
-
-import com.achieveplus.RewardAchievementSystem.entity.Reward;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RewardRepository extends JpaRepository<Reward, Long> {
-}
--- RewardAchievementSystem(back end)/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.achieveplus.RewardAchievementSystem.repository;
-
-import com.achieveplus.RewardAchievementSystem.entity.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
