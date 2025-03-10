1cafbc489732deaadc15897cc4f4876801a17aba
Devdun Fernando <devfernando222@gmail.com>
2025-03-10 14:55:00 +0530

    Comments for Mapper classes

--- RewardAchievementSystem(back end)/src/main/java/com/achieveplus/RewardAchievementSystem/mapper/RewardMapper.java
+++ RewardAchievementSystem(back end)/src/main/java/com/achieveplus/RewardAchievementSystem/mapper/RewardMapper.java
@@ -3,7 +3,18 @@ package com.achieveplus.RewardAchievementSystem.mapper;
 import com.achieveplus.RewardAchievementSystem.dto.RewardDTO;
 import com.achieveplus.RewardAchievementSystem.entity.Reward;
 
+/**
+ * Utility class for mapping Reward entity objects to RewardDTO objects.
+ * This helps in separating entity logic from DTO logic, improving maintainability.
+ */
 public class RewardMapper {
+    /**
+     * Converts a Reward entity into a RewardDTO.
+     * This method delegates the mapping logic to the RewardDTO class.
+     *
+     * @param reward The Reward entity to be converted.
+     * @return A RewardDTO representation of the given Reward entity.
+     */
     public static RewardDTO toDTO(Reward reward) {
         return RewardDTO.fromEntity(reward);
     }
--- RewardAchievementSystem(back end)/src/main/java/com/achieveplus/RewardAchievementSystem/mapper/TaskMapper.java
+++ RewardAchievementSystem(back end)/src/main/java/com/achieveplus/RewardAchievementSystem/mapper/TaskMapper.java
@@ -4,6 +4,12 @@ import com.achieveplus.RewardAchievementSystem.dto.TaskDTO;
 import com.achieveplus.RewardAchievementSystem.entity.Task;
 
 public class TaskMapper {
+    /**
+     * Converts a Task entity object to a TaskDTO (Data Transfer Object).
+     *
+     * @param task The Task entity object to be converted.
+     * @return A TaskDTO containing the information from the provided Task entity.
+     */
     public static TaskDTO toDTO(Task task) {
         TaskDTO dto = new TaskDTO();
         dto.setId(task.getId());
