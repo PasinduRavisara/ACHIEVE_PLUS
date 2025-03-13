42d5483646e17c86dbc6db54a3d7f7ebad8d97c7
Devdun Fernando <devfernando222@gmail.com>
2025-03-13 14:51:17 +0530

    Update Sidebar.js

--- rewardsAchievementsSystem-frontend/src/components/Sidebar.js
+++ rewardsAchievementsSystem-frontend/src/components/Sidebar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Sidebar.css";
+import { Tooltip } from "react-tooltip";
 
 const Sidebar = () => {
   return (
@@ -11,27 +12,40 @@ const Sidebar = () => {
         <span className="logo-text">ACHIEVE+</span>
       </div>
       <div className="nav-links">
-        <div className="nav-item active">
+        <div className="nav-item active" data-tooltip-id="home-tooltip">
           <i className="bi bi-house-door-fill"></i>
         </div>
-        <div className="nav-item">
+        <Tooltip id="home-tooltip" place="right" content="Home" />
+
+        <div className="nav-item" data-tooltip-id="tasks-tooltip">
           <i className="bi bi-list-check"></i>
         </div>
-        <div className="nav-item">
+        <Tooltip id="tasks-tooltip" place="right" content="Tasks" />
+
+        <div className="nav-item" data-tooltip-id="analytics-tooltip">
           <i className="bi bi-bar-chart-fill"></i>
         </div>
-        <div className="nav-item">
+        <Tooltip id="analytics-tooltip" place="right" content="Analytics" />
+
+        <div className="nav-item" data-tooltip-id="ideas-tooltip">
           <i className="bi bi-lightbulb"></i>
         </div>
-        <div className="nav-item">
+        <Tooltip id="ideas-tooltip" place="right" content="Ideas" />
+
+        <div className="nav-item" data-tooltip-id="users-tooltip">
           <i className="bi bi-people-fill"></i>
         </div>
-        <div className="nav-item">
+        <Tooltip id="users-tooltip" place="right" content="Users" />
+
+        <div className="nav-item" data-tooltip-id="dashboard-tooltip">
           <i className="bi bi-grid-1x2-fill"></i>
         </div>
-        <div className="nav-item">
+        <Tooltip id="dashboard-tooltip" place="right" content="Dashboard" />
+
+        <div className="nav-item" data-tooltip-id="settings-tooltip">
           <i className="bi bi-gear-fill"></i>
         </div>
+        <Tooltip id="settings-tooltip" place="right" content="Settings" />
       </div>
     </div>
   );
